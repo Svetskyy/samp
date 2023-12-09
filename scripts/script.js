eb1ad8df798c7cdd231388093690383ee16d3df1
Svetskyy <84508480+Svetskyy@users.noreply.github.com>
2023-12-09 21:18:19 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -499,7 +499,7 @@ async function runGeneticAlgorithm() {
       console.error(e); // For debugging
       toggleLoading(false);
     }
-  }, 1000);
+  }, 2000);
 }async function regenerateAlgo() {
   try {
     // Show loading overlay
